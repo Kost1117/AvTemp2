aa08cf77c53dc7cd5e5f4271f0072cbbf56c9dde
Kost1117 <44008187+Kost1117@users.noreply.github.com>
2018-10-10 23:40:57 +0300

    Initial AvTemp program from White belt.

--- src/AvTemp2.cpp
+++ src/AvTemp2.cpp
@@ -7,9 +7,48 @@
 //============================================================================
 
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+int AverageNumber (const vector<int>& v)
+{
+	int sum;
+	for (auto i : v)
+	{
+		sum += i;
+	}
+	int avn = sum / v.size();
+	return avn;
+}
+
+int main()
+{
+	int Q;
+	cin >> Q;
+	int sum = 0;
+	vector<int> temp(Q);
+
+	for (int& i : temp)
+	{
+		cin >> i;
+		sum += i;
+	}
+	int avtemp = sum / Q;
+	vector<int> result;
+
+	for (int i = 0; i < Q; ++i)
+	{
+		if (temp[i] > avtemp)
+		{
+			result.push_back(i);
+		}
+	}
+	cout << result.size() << endl;
+
+	for (auto i : result)
+	{
+		cout << i << " ";
+	}
 	return 0;
 }
